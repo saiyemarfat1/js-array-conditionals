d98b8a4c8264f71a4f3075c2a37caca8ca121a08
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-29 23:56:38 +0600

    Multi stage condition and nested conditions

---
+++ multi-stage-conditions.js
@@ -0,0 +1,16 @@
+var money = 68;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 25;
+if(danishPrice < money){
+    console.log('Dan Dan danish khamu');
+}
+else if (butterBread < money) {
+    console.log('Butter bon khamu')
+}
+else if (toastBiscuit < money){
+    console.log('chubai chubai toast biscuit khamu')
+}
+else{
+    console.log('khali cha e saster jnno valo')
+}
---
+++ nasted-conditions.js
@@ -0,0 +1,23 @@
+var math = true;
+var geometry = true;
+var stratightLine = false;
+
+
+if (math == true) {
+    if (geometry == true) {
+        if(stratightLine == true){
+
+        }
+        else{
+            console.log('baka phote cholbena')
+        }
+    }
+    else {
+        console.log('pithagrous hite parba na')
+    }
+}
+
+
+else {
+
+}
