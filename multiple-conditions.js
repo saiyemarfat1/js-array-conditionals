ece9b531e122fe2245a15dbe5213323840bffa76
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-29 23:43:09 +0600

    Handle multiple conditions, and or

---
+++ multiple-conditions.js
@@ -0,0 +1,35 @@
+// var isGraduated = true;
+// var cars = 1;
+// var salary = 75000;
+// if(isGraduated === true){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor kopale biye nai')
+// }
+// if(isGraduated === true && salary > 50000){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor kopale biye nai')
+// }
+// if(isGraduated === true && salary > 50000 && cars >=1){
+//     console.log('Eso biye kore feli')
+// }
+// else{
+//     console.log('tor kopale biye nai')
+// }
+
+var isGraduated = false;
+var cars = 0;
+var salary = 61000;
+
+if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('Eso Prem kori');
+}
+else{
+    console.log('tomar friend er mobile number daw')
+}
+if( (isGraduated === true && salary) > 50000 || cars >=1){
+
+}
