564abb066831a0209661282220ae233a4885e252
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-29 21:50:13 +0600

    Declare Array, array length and array index

---
+++ array.js
@@ -0,0 +1,6 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+
+var picnicFee = [5000, 2000, 400, 150];
+var nayikas = ['mahi', 'apu', 'sabnoor', 'sabana']
+var vowels = ['a', 'e', 'i', 'u'];
+console.log(nayikas);
